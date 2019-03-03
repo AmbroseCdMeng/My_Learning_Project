00ee250af792d3891e037c2491992f28ef881a89
MengChengdu <mcd.951026@foxmail.com>
2019-03-03 16:03:41 +0800

    .Net MVC Project --> MatchIDCard

---
+++ 08.NET MVC Project_MatchIDCard/MatchIDCard/MatchIDCard/Areas/IdCard/Controllers/GetIdCardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MatchIDCard.Areas.IdCard.Controllers
+{
+    public class GetIdCardController : Controller
+    {
+        // GET: IdCard/GetIdCard
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ 08.NET MVC Project_MatchIDCard/MatchIDCard/MatchIDCard/Areas/IdCard/IdCardAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace MatchIDCard.Areas.IdCard
+{
+    public class IdCardAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "IdCard";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "IdCard_default",
+                "IdCard/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
